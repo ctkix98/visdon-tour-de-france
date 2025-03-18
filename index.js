1593274b5bec78da6b3448ad90a418f23dfd9bd2
RobinChappalley <robinchappalley@gmail.com>
2025-03-18 15:58:24 +0100

    Add new dependency

---
+++ index.js
@@ -0,0 +1,5 @@
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+const drapeau = document.querySelector(".drapeau");
+drapeau.innerHTML = `<span class="fi fi-fr"></span> <span class="fi fi-gr fis"></span>`;
+
+console.log("coucou");
