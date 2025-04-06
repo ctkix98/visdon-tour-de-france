64f6dd6c07ccc8b2433acfa8ae3e100aeb3fe52f
agathe makumbi <makagathe7@gmail.com>
2025-04-06 09:37:26 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import "/node_modules/flag-icons/css/flag-icons.min.css";
+import "/sections/menu.js"
 const drapeau = document.querySelector(".drapeau");
 drapeau.innerHTML = `<span class="fi fi-fr"></span> <span class="fi fi-gr fis"></span>`;
 
