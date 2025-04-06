0b35d9a1b7cb00439a70feee7d1aaece38960c9c
agathe makumbi <makagathe7@gmail.com>
2025-04-06 11:42:24 +0200

    creation des section du menu

--- sections/menu.js
+++ sections/menu.js
@@ -1,22 +1,22 @@
 import { select } from "d3-selection";
 
-    
-    const svg = select('section#menu')
+    const  container = 
+    const svg = select('section#menu-section')
         .selectAll('svg')
         .data([1])
         .join('svg')
-        .attr('width',1000)
-        .attr('height',1000);
+        .attr('width',svg.clientWidth)
+        .attr('height',845);
 
-    const data = [{x:20, y:20}];
-
-    svg 
+    const data = [{x:477, y:156}];
+    
+   svg 
         .selectAll('circle')// on select aussi pour créer qqch
         .data(data)
         .join('circle')//car maintenant le cercle est crée
         .attr('cx', (d)=> d.x) //position x
         .attr('cy', (d)=> d.y) //position y
-        .attr('r',20); // rayon
+        .attr('r',174); // rayon
 
 
         
