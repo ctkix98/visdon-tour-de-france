657855529b684ad0078ba99377cd57d41a8d063b
agathe makumbi <makagathe7@gmail.com>
2025-04-07 10:41:40 +0200

    modif du css

--- sections/menu.js
+++ sections/menu.js
@@ -1,19 +1,6 @@
 import { select } from "d3-selection";
 
-<<<<<<< HEAD
     
-    const svg = select('section#menu')
-        .selectAll('svg')
-        .data([1])
-        .join('svg')
-        .attr('width',1000)
-        .attr('height',1000);
-
-    const data = [{x:20, y:20}];
-
-    svg 
-=======
-    const  container = 
     const svg = select('section#menu-section')
         .selectAll('svg')
         .data([1])
@@ -24,17 +11,12 @@ import { select } from "d3-selection";
     const data = [{x:477, y:156}];
     
    svg 
->>>>>>> menu-2
         .selectAll('circle')// on select aussi pour créer qqch
         .data(data)
         .join('circle')//car maintenant le cercle est crée
         .attr('cx', (d)=> d.x) //position x
         .attr('cy', (d)=> d.y) //position y
-<<<<<<< HEAD
-        .attr('r',20); // rayon
-=======
         .attr('r',174); // rayon
->>>>>>> menu-2
 
 
         
