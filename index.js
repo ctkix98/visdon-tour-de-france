6281896376b59a8c31adcf20fa4c17ed64835da8
RobinChappalley <robinchappalley@gmail.com>
2025-04-10 11:16:13 +0200

    Create environnement to dev quietely

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import "/node_modules/flag-icons/css/flag-icons.min.css";
-import "/sections/menu.js"
-const drapeau = document.querySelector(".drapeau");
-drapeau.innerHTML = `<span class="fi fi-fr"></span> <span class="fi fi-gr fis"></span>`;
+import "/sections/menu.js";
+import "/sections/map/setup-map.js";
+
+// const drapeau = document.querySelector(".drapeau");
+// drapeau.innerHTML = `<span class="fi fi-fr"></span> <span class="fi fi-gr fis"></span>`;
 
 console.log("coucou");
