5976817d0328d588acbd7d88ae9fb26e67a218dc
RobinChappalley <robinchappalley@gmail.com>
2025-04-10 12:19:56 +0200

    Simplify geojson to have better loading performance

--- sections/map/setup-map.js
+++ sections/map/setup-map.js
@@ -2,29 +2,30 @@
 import { geoMercator, geoPath } from "d3-geo";
 import { select } from "d3-selection";
 import { json } from "d3-fetch";
+import simplify from "@turf/simplify";
 
 Promise.all([
-  json("../../data/map/france.geojson"),
-  json("../../data/map/italy.geojson"),
-]).then(([france, italie]) => {
+  json("../../data/map/france_italie_simplified.geojson")]).then(([data]) => {
   // Créer un élément SVG
   const width = 800;
   const height = 600;
   const svg = select("#map-section")
-    .append("svg")
+    .insert("svg", "img")
     .attr("width", width)
     .attr("height", height);
 
   // Concaténer les données
-  const data = {
-    type: "FeatureCollection",
-    features: france.features.concat(italie.features),
-  };
+
 
   // Créer un générateur de chemin
   const projection = geoMercator().fitSize([width, height], data);
   const path = geoPath().projection(projection);
 
+ console.log(data.features)
+
+  // var options = { tolerance: 0.01, highQuality: false };
+
+  // var simple = simplify(data.features)
   // Ajouter les chemins (les régions) à l'élément SVG
   svg
     .selectAll("path")
@@ -32,8 +33,9 @@ Promise.all([
     .enter()
     .append("path")
     .attr("d", path)
-    .attr("fill", "lightblue")
-    .attr("stroke", "black");
+    .attr("class", "fill-yellow-500 stroke-1 stroke-sky-900");
+  // .attr("class", "stroke-1")
+  // .attr("stroke", "black")
 });
 
-document.querySelector(".france-map").remove()
+document.querySelector(".france-map").remove();
