884faaa88b75ef87c6b72161f3a26ca95fa74c83
RobinChappalley <robinchappalley@gmail.com>
2025-04-11 09:00:15 +0200

    Correct implementation of the promise to get geojson data

--- sections/map/setup-map.js
+++ sections/map/setup-map.js
@@ -2,40 +2,37 @@
 import { geoMercator, geoPath } from "d3-geo";
 import { select } from "d3-selection";
 import { json } from "d3-fetch";
-import simplify from "@turf/simplify";
 
-Promise.all([
-  json("../../data/map/france_italie_simplified.geojson")]).then(([data]) => {
-  // Créer un élément SVG
-  const width = 800;
-  const height = 600;
-  const svg = select("#map-section")
-    .insert("svg", "img")
-    .attr("width", width)
-    .attr("height", height);
+const createMap = () => {
+  json("../../data/map/france_italie_simplified.geojson").then((data) => {
+    // Créer un élément SVG
+    const width = 800;
+    const height = 600;
+    const svg = select("#map-section")
+      .insert("svg", "img")
+      .attr("width", width)
+      .attr("height", height);
 
-  // Concaténer les données
+    // Concaténer les données
 
+    // Créer un générateur de chemin
+    const projection = geoMercator().fitSize([width, height], data);
+    const path = geoPath().projection(projection);
 
-  // Créer un générateur de chemin
-  const projection = geoMercator().fitSize([width, height], data);
-  const path = geoPath().projection(projection);
+    console.log(data.features);
 
- console.log(data.features)
+    // Ajouter les chemins (les régions) à l'élément SVG
+    svg
+      .selectAll("path")
+      .data(data.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .attr("class", "fill-yellow-500 stroke-[0.05] stroke-sky-900");
+    // .attr("class", "stroke-1")
+    // .attr("stroke", "black")
+  });
+};
 
-  // var options = { tolerance: 0.01, highQuality: false };
-
-  // var simple = simplify(data.features)
-  // Ajouter les chemins (les régions) à l'élément SVG
-  svg
-    .selectAll("path")
-    .data(data.features)
-    .enter()
-    .append("path")
-    .attr("d", path)
-    .attr("class", "fill-yellow-500 stroke-1 stroke-sky-900");
-  // .attr("class", "stroke-1")
-  // .attr("stroke", "black")
-});
-
+createMap()
 document.querySelector(".france-map").remove();
