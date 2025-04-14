8ccaf6d2d872644e0bdff7b4f0287ad05e6fa376
agathe makumbi <makagathe7@gmail.com>
2025-04-14 20:07:47 +0200

    Menu.js placed in menu folder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import "/node_modules/flag-icons/css/flag-icons.min.css";
-import "/sections/menu.js"
+import "/sections/menu/menu.js"
 const drapeau = document.querySelector(".drapeau");
 drapeau.innerHTML = `<span class="fi fi-fr"></span> <span class="fi fi-gr fis"></span>`;
 
