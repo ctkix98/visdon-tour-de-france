290df0cff29627c656b98c81d5f09b1ea4ce8cc6
agathe makumbi <makagathe7@gmail.com>
2025-04-17 09:04:30 +0200

    menu section 1 css done

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 import "/sections/menu/menu.js"
-const drapeau = document.querySelector(".drapeau");
-drapeau.innerHTML = `<span class="fi fi-fr"></span> <span class="fi fi-gr fis"></span>`;
+import "/custom-elements/menu-bubble.js";
+import { displayCategories } from "./sections/menu/menu";
+import { displaySection } from "./helpers";
+
+/* const drapeau = document.querySelector(".drapeau");
+drapeau.innerHTML = `<span class="fi fi-fr"></span> <span class="fi fi-gr fis"></span>`; */
 
 console.log("coucou");
+
+
+const routes = () => {
+    const hash = window.location.hash || '#menu-section';
+    const hashs = hash.split('-')
+  
+    switch(hashs[0]) {
+      case '#menu':
+          displaySection('menu')
+          displayCategories();
+      break;
+
+      case '#stakes':
+          displaySection('stakes')
+          //displayCategories();
+      break;
+
+      case '#equipment':
+          displaySection('equipment')
+          //displayCategories();
+      break;
+
+      case '#route':
+        displaySection('route')
+        //displayCategories();
+      break;
+
+      case '#rules':
+        displaySection('rules')
+        //displayCategories();
+      break;
+    }
+  }
+
+  // On veut être averti des changements
+  window.addEventListener('hashchange', routes)
+  routes()
