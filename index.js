68de2fa7184a06bbc9a38fd895802e815e52c777
RobinChappalley <robinchappalley@gmail.com>
2025-04-28 14:40:37 +0200

    Remove double defer and remove useless call

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 import "/sections/menu.js";
-import "/sections/map/setup-map.js";
+// import "/sections/map/setup-map.js";
+import "/sections/map/setup-traces.js";
 
 // const drapeau = document.querySelector(".drapeau");
 // drapeau.innerHTML = `<span class="fi fi-fr"></span> <span class="fi fi-gr fis"></span>`;
