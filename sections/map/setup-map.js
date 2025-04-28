22e0e526ee82d50c9ca005a83c79f70b6beb4c43
RobinChappalley <robinchappalley@gmail.com>
2025-04-28 16:02:27 +0200

    Correct width of the map to be the biggest possible without overflowing

--- sections/map/setup-map.js
+++ sections/map/setup-map.js
@@ -3,15 +3,17 @@ import { geoMercator, geoPath } from "d3-geo";
 import { select } from "d3-selection";
 import { json } from "d3-fetch";
 
-const width = window.innerWidth;
+const width = document.documentElement.clientWidth;
 const height = window.innerHeight;
+console.log(width);
 export const createMap = () => {
   return new Promise((resolve) => {
     json("../../data/map/france_italie_simplified.geojson").then((data) => {
       const svg = select("#map-section")
         .insert("svg", "img")
-        .attr("width", width)
-        .attr("height", height);
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("viewBox", `0 0 ${width} ${height}`);
 
       const projection = geoMercator().fitSize([width, height], data);
       const path = geoPath().projection(projection);
