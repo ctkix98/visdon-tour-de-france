043d88c5172f33c09d0f677c814a442651c4d7f2
agathe makumbi <makagathe7@gmail.com>
2025-04-28 17:37:45 +0200

    Section satkes fully implemented and maillot.json updated

---
+++ sections/menu/stakes.js
@@ -0,0 +1,30 @@
+// recupérer les informations du fichier json
+import jerseyJson from '../../data/maillots.json' assert { type: 'json' };
+
+// Récupérer le tag contenant la liste des cards 
+const jerseyInfoContainer = document.querySelector('div#jersey-info-container');
+
+
+
+const displayJerseyDetails = async(id) => {
+    console.log(id);
+    // vider le contenu de l'élément 
+    jerseyInfoContainer.innerHTML = '';
+    const jerseySelected =  jerseyJson.find(jersey => jersey.id == id);
+    console.log(jerseySelected);
+
+    if (jerseySelected) {
+    
+        const jerseyInfoCard = document.createElement('jersey-info-card');
+        jerseyInfoCard.setAttribute('title', jerseySelected.titre);
+        jerseyInfoCard.setAttribute('content', jerseySelected.contenu);
+        jerseyInfoCard.setAttribute('img', jerseySelected.img);
+        jerseyInfoCard.setAttribute('class', 'jersey-information');
+        jerseyInfoContainer.appendChild(jerseyInfoCard);
+    
+       
+    }   
+    
+}
+
+export {displayJerseyDetails};
