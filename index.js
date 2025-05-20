ecd2a7182340bc622d0f6b43a85addc4f35f4a22
Robin Chappalley <robin@chapi.ch>
2025-05-20 15:03:01 +0200

    Remove useless import

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import "/sections/map/setup-traces.js"
 import "/sections/menu/stakes.js"
 import "/sections/menu/teams.js"
 import "/sections/menu/route.js"
-import "/sections/menu/setup-curve.js"
+// import "/sections/menu/setup-curve.js"
 
 import { displayCategories } from "./sections/menu/menu";
 import { displaySection, activateLink, toggleMenu } from "./helpers";
@@ -57,7 +57,7 @@ const routes = () => {
 
     case "#route":
       displaySection("route");
-      // createElevationCurve();
+      createElevationCurve();
       console.log("courbe créée depuis index")
       toggleMenu()
       break;
